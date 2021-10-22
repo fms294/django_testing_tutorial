bbbe82002099b7f961f5e9629114845069c37b43
Fantamady Sylla <fms294@nyu.edu>
2021-10-22 12:43:40 -0400

    First Commit

---
+++ testing-tutorial/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/fantamady/Desktop/django-testing-tutorial/budgetproject/testing-tutorial/bin/python3.9
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
